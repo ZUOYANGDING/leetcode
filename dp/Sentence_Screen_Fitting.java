eb248aba8703f647d38a2daaef9b7cb74a4e4ee9
zuoyangding <zuoyangding@yahoo.com>
2019-10-24 15:27:12 -0700

    Sentence_Screen_Fitting

---
+++ dp/Sentence_Screen_Fitting.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int wordsTyping(String[] sentence, int rows, int cols) {
+        int res = 0;
+        int l = sentence.length;
+        int[] count = new int[l];
+        int index = 0;
+        for (String word : sentence) {
+            if (cols < word.length()) {
+                return res;
+            } else {
+                count[index] = word.length();
+                index++;
+            }
+        }
+        int r = 0, c = 0;
+        index = 0;
+        while (r < rows) {
+            if (index == l) {
+                res++;
+                index = 0;
+            }
+            if (c+count[index] > cols || c>=cols) {
+                c = 0;
+                r++;
+            } else {
+                c = c + count[index] + 1;
+                index++;
+            }
+        }
+        return res;
+    }
+}
